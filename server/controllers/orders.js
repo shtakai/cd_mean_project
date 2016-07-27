480bf9bd8312fea00049eba98b58ef73ef7d2e2c
Alyson Takai <shtakai@gmail.com>
2016-07-27 16:43:59 -0700

    fixed for getting summary

--- server/controllers/orders.js
+++ server/controllers/orders.js
@@ -18,6 +18,8 @@ module.exports = {
     let order = new Order(req.body)
     const user_id = req.session['user_info']['id']
     console.log('user_id', user_id)
+    order.state = 'AVAILABLE'
+    console.log('order---', order)
     User.findOne({_id: user_id})
       .then( (user) => {
         order._user = user
@@ -38,24 +40,72 @@ module.exports = {
       } )
   }, //end create
   index: (req, res) =>{
+    const sell = Order.aggregate([
+      {
+        $match: {
+          orderType: 'SELL',
+          traded: false,
+          canceled: false,
+          state: 'AVAILABLE',
+        }
+      },
+      {
+        $group: {
+          _id: '$price',
+          count: {$sum: "$amount"}
+        }
+      },
+      {
+        $project: {
+          _id: 1,
+          count: 1,
+          price: 1,
+        }
+      }
+    ])
+
+    const buy = Order.aggregate([
+      {
+        $match: {
+          orderType: 'BUY',
+          traded: false,
+          canceled: false,
+          state: 'AVAILABLE',
+        }
+      },
+      {
+        $group: {
+          _id: '$price',
+          count: {$sum: "$amount"}
+        }
+      },
+      {
+        $project: {
+          _id: 1,
+          count: 1,
+          price: 1,
+        }
+      }
+    ])
+
     let sellOrders
     let buyOrders
-    Order.find({traded: false, canceled: false, orderType: 'SELL'})
-      .sort('-created_at')
+    //Order.find({traded: false, canceled: false, orderType: 'SELL', state: 'AVAILABLE'})
+    sell.exec()
+      //.sort('-created_at')
       .then( (_sellOrders) => {
         console.log('sell order', _sellOrders)
         sellOrders = _sellOrders
         return _sellOrders
       } )
       .then( () => {
-        Order.find({traded:false, canceled: false, orderType: 'BUY'})
+        //Order.find({traded:false, canceled: false, orderType: 'BUY' ,state: 'AVAILABLE'})
+        buy.exec()
           .then( (_buyOrders) => {
             buyOrders = _buyOrders
             console.log('finished grabbed orders')
             res.json({status: true, buyOrders: buyOrders, sellOrders: sellOrders})
           } )
-
-
       } )
       .catch( (err) => {
         console.log('error', err)
