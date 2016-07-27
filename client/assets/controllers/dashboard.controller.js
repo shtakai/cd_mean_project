d35b0a1c1975842d02a8ebc8a3b099f734b599b7
Alyson Takai <shtakai@gmail.com>
2016-07-27 16:45:27 -0700

    login

---
+++ client/assets/controllers/dashboard.controller.js
@@ -0,0 +1,15 @@
+
+(function () {
+  'use strict'
+
+  angular
+    .module('app')
+    .controller('DashboardController', Controller)
+
+  function Controller ($location, $routeParams) {
+    var _this = this
+
+    console.log('dashboard')
+
+
+}})()
