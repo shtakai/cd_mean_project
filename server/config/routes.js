48448eb4e26efc9a197716c9fb905eca5bfade84
Alyson Takai <shtakai@gmail.com>
2016-07-27 13:10:23 -0700

    modified route

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,15 +1,12 @@
 console.log('read routes')
 const users = require('./../controllers/users.js')
-const polls = require('./../controllers/polls.js')
+const orders = require('./../controllers/orders.js')
 
 module.exports = (app) => {
   app.post('/login', users.login)
   app.get('/logout', users.logout)
   app.get('/session', users.session)
 
-  app.get('/polls/:id', polls.show)
-  app.get('/polls', polls.index)
-  app.post('/polls', polls.create)
-  app.delete('/polls/:id', polls.destroy)
-  app.get('/options/:id/vote', polls.vote)
+  app.get('/orders', orders.index)
+  app.post('/orders', orders.create)
 }
