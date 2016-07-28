434ba0703ee14be3934c4d34e247657726b3f6fa
Alyson Takai <shtakai@gmail.com>
2016-07-27 20:33:27 -0700

    order#create

--- client/assets/controllers/dashboard.controller.js
+++ client/assets/controllers/dashboard.controller.js
@@ -6,10 +6,24 @@
     .module('app')
     .controller('DashboardController', Controller)
 
-  function Controller ($location, $routeParams) {
+  function Controller (OrderFactory, UserFactory, $location, $routeParams) {
     var _this = this
+    var sellChart
+    var buyChart
+    var chart
 
     console.log('dashboard')
+
+    _this.getOrders = function(){
+      console.log('dashboard#getOrders')
+      OrderFactory.getOrders((data) =>{
+        console.log('data', data)
+        if(data.status){
+          _this.orders = data.orders
+        }
+      })
+
+    }
 
 
 }})()
